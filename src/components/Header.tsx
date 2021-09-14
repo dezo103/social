8061d976e445db09dfea90ea488c3c2a3f48d91d
Aliaksandr Rudyk <dezo103@gmail.com>
2021-09-14 16:43:42 +0300

    init: start project

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo%21_Logo.svg/1200px-Logo%21_Logo.svg.png"
+                alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM" alt=""/>
+            </div>
+            <div>
+                Ava + description
+            </div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
