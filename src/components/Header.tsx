9ad6977a8131d7c74f18c5b323c7d3d6384f42f4
Aliaksandr Rudyk <dezo103@gmail.com>
2021-09-14 22:06:29 +0300

    feat: add css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={s.header}>
             <img
                 src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Logo%21_Logo.svg/1200px-Logo%21_Logo.svg.png"
                 alt=""/>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div>
                 <img src="https://media-exp1.licdn.com/dms/image/C4D1BAQGDmALg_8s-Yg/company-background_10000/0/1519799119530?e=2159024400&v=beta&t=4WV9YKR9L3PAEnppWmPPMk5xVnETtWvhZN8NexEzPwM" alt=""/>
             </div>
@@ -12,9 +13,9 @@ const Profile = () => {
             <div>
                 My posts
                 <div>New post</div>
-                <div>
-                    <div>post1</div>
-                    <div>post2</div>
+                <div className="posts">
+                    <div className={s.item}>post1</div>
+                    <div className={s.item}>post2</div>
                 </div>
             </div>
         </div>
