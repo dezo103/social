9f0866ef487877021f912dfe5ef69de942b4445a
Aliaksandr Rudyk <dezo103@gmail.com>
2021-09-23 23:39:26 +0300

    feat: add Dialogs columns

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import s from './Dialogs.module.css';
+
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Dimych</div>
+                <div className={s.dialog}>Artem</div>
+                <div className={s.dialog}>Sanya</div>
+                <div className={s.dialog}>Julia</div>
+                <div className={s.dialog}>Loshara</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
