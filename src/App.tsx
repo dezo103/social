5ba158300598d7981bd2bc6e0f37e00ef7f96642
Aliaksandr Rudyk <dezo103@gmail.com>
2021-12-15 00:08:24 +0300

    delete commented code

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,8 @@ import {Route} from 'react-router-dom';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import {RootStateType} from "./redux/redux-store";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
-// type AppPropsType = {
-//     store: RootStateType
-// }
-
-// const App: React.FC<AppPropsType> = (props: AppPropsType) => {
 const App = () => {
     return (
         <div className='app-wrapper'>
@@ -22,8 +16,6 @@ const App = () => {
             <Navbar/>
             <div className="app-wrapper-content">
                 <Route path={'/dialogs'} render={() => <DialogsContainer />}/>
-                {/*<Route path={'/dialogs'} render={() => <DialogsContainer store={props.store}/>}/>*/}
-                {/*<Route path={'/profile'} render={() => <Profile store={props.store}/>}/>*/}
                 <Route path={'/profile'} render={() => <Profile />}/>
                 <Route path={'/news'} render={() => <News/>}/>
                 <Route path={'/music'} render={() => <Music/>}/>
--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import MyPosts from "./MyPosts/MyPosts";
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {ActionsTypes, postDataType, StoreType} from "../../redux/store";
-import {RootStateType} from "../../redux/redux-store";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
-
-// type ProfilePostType = {
-//     store: RootStateType
-// }
 
 const Profile = () => {
     return (
         <div>
             <ProfileInfo/>
-            {/*<MyPostsContainer store = {props.store}/>*/}
             <MyPostsContainer />
-            {/*<MyPosts postData={props.postData}*/}
-            {/*         newPostText={props.newPostText}*/}
-            {/*         dispatch={props.dispatch}/>*/}
         </div>
     )
 }
