ddea4746bab8129c6bd49acab0f29727688b0ba0
Aliaksandr Rudyk <dezo103@gmail.com>
2022-04-19 19:48:32 +0300

    add useState for editableStatusSpan

--- src/components/Profile/ProfileInfo/ProfileStatus.tsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.tsx
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import s from './ProfileInfo.module.css';
 
 
 const ProfileStatus = (props: any) => {
 
-    let state = {
-        editMode: false
+    let [state, setState] = useState({editMode: false})
+
+    let activateEditMode = () => {
+        setState({
+            editMode: true
+        })
+    }
+
+    let deactivateEditMode = () => {
+        setState({
+            editMode: false
+        })
     }
 
     return (
         <div>
             {!state.editMode
                 ? <div>
-                    <span>{props.status}</span>
+                    <span onDoubleClick={activateEditMode}>{props.status}</span>
                 </div>
                 : <div>
-                    <input value={props.status}></input>
+                    <input
+                        value={props.status}
+                        onBlur={deactivateEditMode}
+                        autoFocus={true}></input>
                 </div>
             }
         </div>
