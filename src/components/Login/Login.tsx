5220548b383c90411972b7661e5162cbd61b4698
Aliaksandr Rudyk <dezo103@gmail.com>
2022-04-19 23:38:13 +0300

    add Redux-form to page

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
-export const Login = () => {
+
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props: any) => {
     return (
-        <h1>
-            Login
-        </h1>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={'login'} name={'login'} component={'input'}  />
+            </div>
+            <div>
+                <Field placeholder={'password'} name={'password'} component={'input'}  />
+            </div>
+            <div>
+                <Field type={'checkbox'} name={'rememberMe'} component={'input'}/>
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     );
 };
+
+const LoginReduxForm = reduxForm<FormDataType>({form: 'login'})(LoginForm)
+
+export const Login = () => {
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
+    return (
+        <div>
+            <h1>
+                Login
+            </h1>
+            <LoginReduxForm onSubmit={onSubmit}/>
+        </div>
+    );
+};
+
+
+
+
 
